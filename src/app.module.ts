755b782bae94bfede61fc28f2f5732a5a035a7cc
scriptSQD <73490413+scriptSQD@users.noreply.github.com>
2022-01-06 01:20:37 +0300

    Change DB access password
    Ruined my old password by leaking it here...

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { Deal, DealSchema } from './schemas/deal.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://sqd:CrB99rI5ZU1WQUZE@cluster0.v4qgc.mongodb.net/SQDBoardsDB?retryWrites=true&w=majority'),
+    MongooseModule.forRoot('mongodb+srv://userapi:1111@cluster0.v4qgc.mongodb.net/SQDBoardsDB?retryWrites=true&w=majority'),
     MongooseModule.forFeature([{name: Deal.name, schema: DealSchema}])
   ],
   controllers: [AppController],
